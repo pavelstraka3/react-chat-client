875abbea9932d2d759b65063d86c42b1ec713493
Pavel Straka <pavel.straka3@seznam.cz>
2025-04-06 08:38:42 +0200

    Simple chat UI demo

---
+++ src/components/ui/chat-message.tsx
@@ -0,0 +1,16 @@
+import {Message} from "@/App.tsx";
+
+type Props = {
+  message: Message
+}
+
+const ChatMessage = ({message}: Props) => {
+  return (
+    <div className={`flex ${message.username === "Pavel" ? "justify-end" : ""}`}>
+      <span className="font-bold">{message.username}: </span>
+      <div className="bg-blue-500 rounded-2xl text-white p-2 ml-2">{message.content}</div>
+    </div>
+  );
+};
+
+export default ChatMessage;
